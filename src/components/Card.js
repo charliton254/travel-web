5be582e44f5202e616c93897b1f56f32daa3b1e4
charliton254 <55441672+charliton254@users.noreply.github.com>
2024-07-09 23:42:40 +0300

    card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,6 +14,28 @@ function Card() {
                             text='Explore the hidden waterfall deep inside the Amazon Jungle'
                             label='Adventure'
                             path='/services' />
+                        <CardItem
+                            src='Images/img-2.jpg'
+                            text='Travel through the Island of Bali in a Private Cruise '
+                            label='Luxury'
+                            path='/services' />
+                    </ul>
+                    <ul className='card-items'>
+                        <CardItem
+                            src='Images/img-3.jpg'
+                            text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+                            label='Mystery'
+                            path='/services' />
+                        <CardItem
+                            src='Images/img-4.jpg'
+                            text='Experience Football on top of the Himalayan Mountains'
+                            label='Adventure'
+                            path='/products' />
+                        <CardItem
+                            src='Images/img-8.jpg'
+                            text='Ride through the Sahara Desert on a guided camel tour'
+                            label='Luxury'
+                            path='/sighn-up' />
                     </ul>
                 </div>
             </div>
